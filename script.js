4e862ca42c941c867e1df0fb80b9dc0f1d5b3078
coesnonh <166711611+coesnonh@users.noreply.github.com>
2024-04-11 13:13:38 +0200

    maj

---
+++ script.js
@@ -0,0 +1,10 @@
+// Exemple : Ajouter une animation de défilement doux pour les liens dans le menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+})
